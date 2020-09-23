ece87b056d77d98aa477d01dcd69dc47d752afe4
javacode2017 <bhujatha.edu@gmail.com>
2020-09-23 20:02:40 +0530

    firstcommit

---
+++ src/main/java/com/arg/exam/MyController.java
@@ -0,0 +1,13 @@
+package com.arg.exam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/hi")
+	public String hello() {
+		return "bbbbbbbbb";
+	}
+}
